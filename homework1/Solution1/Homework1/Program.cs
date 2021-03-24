ae7ee6d9af9768c832877fc2d52cd2192a8e62f6
Stefanija888 <58893714+Stefanija888@users.noreply.github.com>
2021-03-24 14:41:28 +0100

    Homework1

---
+++ homework1/Solution1/Homework1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ReadLine();
+            Console.WriteLine("Hello World!");
+           
+        }
+    }
+}
